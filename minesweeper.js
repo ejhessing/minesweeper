94f298f8e4d6d232a4dbea14d598f2b6c5810e1a
Erwin <ejhessing@gmail.com>
2016-06-20 17:47:01 +1200

    GetRow function added

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', startGame)
+var board = {
+	cells:[]
+};
+
 
 function startGame () {
   var board = document.getElementsByClassName('board')[0].children;
@@ -6,6 +10,7 @@ function startGame () {
   for(var i = 0; i < board.length; i++) {
     board[i].addEventListener('click',showCell);
     board[i].addEventListener('contextmenu',markCell);
+    getRow(board[i]);
   }
 }
 
@@ -18,4 +23,13 @@ function showCell (evt) {
 function markCell (evt) {
   evt.preventDefault();
   evt.target.classList.toggle('marked');
+}
+
+function getRow (elements) {
+	var classList = elements.classList;
+	for(var i = 0; i< classList.length; i++){
+		if(classList[i].charAt(0) === 'r'){
+			return Number(classList[i].charAt(classList[i].length-1));
+		}
+	}
 }
