6d86742e6f583e3a80166f9ee1ad283ce2dcff8f
Erwin <ejhessing@gmail.com>
2016-06-20 12:08:41 +1200

    Added Event listener to right click

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,21 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 function startGame () {
+  var board = document.getElementsByClassName('board')[0].children;
+
+  for(var i = 0; i < board.length; i++) {
+    board[i].addEventListener('click',showCell);
+    board[i].addEventListener('contextmenu',markCell);
+  }
+}
+
+
+function showCell (evt) {
+  evt.target.classList.remove('hidden');
+}
+
+
+function markCell (evt) {
+  evt.preventDefault();
+  evt.target.classList.toggle('marked');
 }
